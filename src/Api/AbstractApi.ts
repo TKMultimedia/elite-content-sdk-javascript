ef94c029061bad0db2374c989831e90e4505dbb2
Ktran <tranthienkhoi@gmail.com>
2020-11-23 16:15:22 +0700

    Updates space

--- src/Api/AbstractApi.ts
+++ src/Api/AbstractApi.ts
@@ -3,42 +3,42 @@ import { get as _get } from 'lodash';
 
 abstract class AbstractApi {
 
-    // --------------------------------------------------------------------------------------------
-    // Protected Properties
-    // --------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------------------
+  // Protected Properties
+  // --------------------------------------------------------------------------------------------
 
-    /**
-     * The HTTP Client
-     * Use this to init all HTTP functionality
-     */
-    protected readonly http: AxiosInstance;
-    protected readonly baseURL: string = 'https://api.eliteprospects.com/v1';
-    private readonly apiKey: string = '';
-    private readonly path: string = '';
+  /**
+   * The HTTP Client
+   * Use this to init all HTTP functionality
+   */
+  protected readonly http: AxiosInstance;
+  protected readonly baseURL: string = 'https://api.eliteprospects.com/v1';
+  private readonly apiKey: string = '';
+  private readonly path: string = '';
 
-    // --------------------------------------------------------------------------------------------
-    // Private Properties
-    // --------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------------------
+  // Private Properties
+  // --------------------------------------------------------------------------------------------
 
-    public constructor(apiKey: string, path: string) {
-        const options: AxiosRequestConfig = {
-            baseURL: this.baseURL
-        };
-        this.apiKey = apiKey;
-        this.path = path;
+  public constructor(apiKey: string, path: string) {
+    const options: AxiosRequestConfig = {
+      baseURL: this.baseURL
+    };
+    this.apiKey = apiKey;
+    this.path = path;
 
-        this.http = Axios.create(options);
-    }
+    this.http = Axios.create(options);
+  }
 
-    protected execute(requestParams: any): AxiosPromise {
-        return this.http.get(this.path, {
-            params: {
-                apiKey: this.apiKey,
-                ...requestParams
-            },
-            transformResponse: (data: string): any => _get(JSON.parse(data), 'data', {})
-        });
-    }
+  protected execute(requestParams: any): AxiosPromise {
+    return this.http.get(this.path, {
+      params: {
+        apiKey: this.apiKey,
+        ...requestParams
+      },
+      transformResponse: (data: string): any => _get(JSON.parse(data), 'data', {})
+    });
+  }
 }
 
 export default AbstractApi;
