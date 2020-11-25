905ff9b29459a6fb6c8b6771d7948b3cc786bab1
Ktran <tranthienkhoi@gmail.com>
2020-11-25 15:24:19 +0700

    Updated player api

--- src/Api/PlayerApi.ts
+++ src/Api/PlayerApi.ts
@@ -10,6 +10,16 @@ class PlayerApi extends AbstractApi {
   public getPlayerInfoById(playerId: number): AxiosPromise<IElitePlayer> {
     return this.execute({}, `${playerId}`);
   }
+
+  public listPlayers(offset?: number, limit?: number): AxiosPromise<IElitePlayer[]> {
+    const processOffset: number = typeof offset !== 'undefined' ? offset : 0;
+    const processLimit: number = typeof limit !== 'undefined' ? limit : 100;
+
+    return this.execute({
+      offset: processOffset,
+      limit: processLimit
+    });
+  }
 }
 
 export default PlayerApi;
